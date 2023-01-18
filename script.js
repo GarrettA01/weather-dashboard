2ff1bd4e05fe96fbe66349fad1555cf3820b8df0
Garrett Anderson <gman.anderson2001@gmail.com>
2023-01-18 02:16:02 -0700

    Simplified header

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var userForm = document.getElementById("form-sbt");
 var weatherApi = "https://api.opemweathermap.org/data/2.5/weather?lat=";
 
 var key = "46256012a49a37d8e104f08913eb594e";
+var key2 = "4a96d5adb653472a52d15b28efbc1ef7";
+
 //Functions
 
 //Event Listeners
