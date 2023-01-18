c758058e14d747900769426064092f9445fc4ea5
Garrett Anderson <gman.anderson2001@gmail.com>
2023-01-18 01:18:30 -0700

    Filled out aside in html

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 //Variables
 var userForm = document.getElementById("user-input");
+var userForm = document.getElementById("form-sbt");
 
+var weatherApi = "https://api.opemweathermap.org/data/2.5/weather?lat=";
 
-
-
-
+var key = "46256012a49a37d8e104f08913eb594e";
 //Functions
-
-
 
 //Event Listeners
